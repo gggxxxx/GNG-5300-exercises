d5274103368c86f335839d1fd6af4127ff69d5d2
gggxxxx <100735184+gggxxxx@users.noreply.github.com>
2022-09-13 19:58:05 -0400

    Add files via upload
    update

---
+++ GNG 5300 1st exercise.py
@@ -0,0 +1,8 @@
+#beelow are coded by Xiong Gu
+a = input("please input the first number:")
+b = input("please input the second number:")
+c = int(a) + int(b)
+print(c)
+
+#To my peer: please add the third input and add it with the number c
+
