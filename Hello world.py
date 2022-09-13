90e14d2826f590981449467a448bd6faf033da7c
gggxxxx <100735184+gggxxxx@users.noreply.github.com>
2022-09-13 19:56:08 -0400

    Add files via upload
    GNG 5300 class exercises

---
+++ Hello world.py
@@ -0,0 +1,43 @@
+'''
+print("Hello World!")
+
+a = 2
+b = 3
+print(a + b)
+
+PI = 3.14
+print(PI)
+PI = 3.15
+print(PI)
+
+my_integer_variable = 1000
+my_float_variable = 3.5
+my_complex_variable = 1 + 2j
+
+print(type(my_integer_variable))
+print(type(my_float_variable))
+print(type(my_complex_variable))
+________________________________
+print(2+1)
+print(5-2)
+print(2*3)
+print(8/4)
+print(2**3)
+print(9%2)
+________________________________
+s1 = "Hello "
+s2 = "World!"
+print(s1 + s2)
+s3 = "%s%s" %(s1,s2)
+print(s3)
+s4 = "".join([s1,s2])
+print(s4)
+'''
+#beelow are coded by Xiong Gu
+a = input("please input the first number:")
+b = input("please input the second number:")
+c = int(a) + int(b)
+print(c)
+
+#To my peer: please add the third input and add it with the number c
+
